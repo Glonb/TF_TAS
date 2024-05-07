865d5cd995e111459f4abd502b5d0c7f9723eb14
wanyao <wanyaocsg17@icloud.com>
2024-05-07 15:03:36 +0800

    Update search_autoformer.sh

--- search_autoformer.sh
+++ search_autoformer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python3 -m torch.distributed.launch --nproc_per_node=1 --use_env search_autoformer.py --data-path './dataset/imagenet' --gp \
+python3 --use_env search_autoformer.py --gp \
  --change_qk --relative_position --dist-eval --cfg './experiments/search_space/space-T.yaml' --output_dir './OUTPUT/search'
 
 
