cda4ffe45c2d889a5d8800a1cfa7c398ca65a836
wanyao <wanyaocsg17@icloud.com>
2024-05-07 15:11:28 +0800

    Update search_autoformer.sh

--- search_autoformer.sh
+++ search_autoformer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python3 search_autoformer.py --gp \
+python3 search_autoformer.py --data-set CIFAR10 --gp \
  --change_qk --relative_position --dist-eval --cfg './experiments/search_space/space-T.yaml' --output_dir './OUTPUT/search'
 
 
