9b1d5a80c99df1fd4f0261c27aef68a5df86ef9f
wanyao <wanyaocsg17@icloud.com>
2024-05-14 15:34:04 +0800

    Update p_utils.py

--- lib/training_free/p_utils.py
+++ lib/training_free/p_utils.py
@@ -56,9 +56,10 @@ def get_layer_metric_array_dss(net, metric, mode):
         if mode == 'channel' and hasattr(layer, 'dont_ch_prune'):
             continue
         if isinstance(layer, nn.Linear) and layer.samples:
-            print(layer.out_features)
+            # print(layer.out_features)
             metric_array.append(metric(layer))
         if isinstance(layer, torch.nn.Linear) and layer.out_features == 10:
+            print('########################')
             metric_array.append(metric(layer))
     return metric_array
 
