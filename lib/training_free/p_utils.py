5968b3508d3d99a03f74bceb5b81589f00461983
wanyao <wanyaocsg17@icloud.com>
2024-05-14 16:08:32 +0800

    Update p_utils.py

--- lib/training_free/p_utils.py
+++ lib/training_free/p_utils.py
@@ -59,7 +59,6 @@ def get_layer_metric_array_dss(net, metric, mode):
             # print(layer.out_features)
             metric_array.append(metric(layer))
         if isinstance(layer, nn.Linear) and layer.out_features == 10:
-            print('########################')
             metric_array.append(metric(layer))
     return metric_array
 
