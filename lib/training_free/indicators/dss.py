e8b46d5fda4789d6309c294bf5c31b1e945cfe5d
wanyao <wanyaocsg17@icloud.com>
2024-05-21 15:10:51 +0800

    Update dss.py

--- lib/training_free/indicators/dss.py
+++ lib/training_free/indicators/dss.py
@@ -38,7 +38,7 @@ def compute_dss_per_weight(net, inputs, targets, mode, split_data=1, loss_fn=Non
                 return torch.abs(layer.sampled_weight.grad * layer.sampled_weight)
             else:
                 return torch.zeros_like(layer.sampled_weight)
-        if isinstance(layer, nn.Linear) and 'qkv' in layer._get_name() and layer.samples \
+        if isinstance(layer, nn.Linear) and 'Attention' in layer._get_name() and layer.samples \
             or isinstance(layer, nn.Linear) and layer.out_features == layer.in_features and layer.samples:
             if layer.samples['weight'].grad is not None:
                 print('Q-K-V矩阵: ', layer.samples['weight'].shape)
