3edf280088a47f59a2cb217472a38350173710d0
wanyao <wanyaocsg17@icloud.com>
2024-05-21 16:42:58 +0800

    Update dss.py

--- lib/training_free/indicators/dss.py
+++ lib/training_free/indicators/dss.py
@@ -32,6 +32,7 @@ def compute_dss_per_weight(net, inputs, targets, mode, split_data=1, loss_fn=Non
     torch.sum(output).backward()
 
     def dss(layer):
+        print('name: ', layer._get_name())
         if layer._get_name() == 'PatchembedSuper':
             print('****************')
             if layer.sampled_weight.grad is not None:
