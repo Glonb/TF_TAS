1b0ef003e8ad3c629af22b73f70b022793ee903a
wanyao <wanyaocsg17@icloud.com>
2024-05-21 16:37:45 +0800

    Update p_utils.py

--- lib/training_free/p_utils.py
+++ lib/training_free/p_utils.py
@@ -55,7 +55,7 @@ def get_layer_metric_array_dss(net, metric, mode):
     for layer in net.modules():
         if mode == 'channel' and hasattr(layer, 'dont_ch_prune'):
             continue
-        if isinstance(layer, nn.Linear) and layer.samples and layer.out_features != 10:
+        if isinstance(layer, nn.Linear) and layer.samples :
             # print(layer.out_features)
             metric_array.append(metric(layer))
         if isinstance(layer, nn.Linear) and layer.out_features == 10:
