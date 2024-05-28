5b1e5889545a0991db34eb85e62b18b95bb2b713
wanyao <wanyaocsg17@icloud.com>
2024-05-28 15:02:50 +0800

    Update compute_indicators.py

--- lib/training_free/compute_indicators.py
+++ lib/training_free/compute_indicators.py
@@ -81,6 +81,7 @@ def find_indicators(net_orig,
     def sum_arr(arr):
         sum = 0.
         for i in range(len(arr)):
+            print(f"arr[{i}].device: {arr[i].device}")
             sum += torch.sum(arr[i])
         return sum.item()
 
