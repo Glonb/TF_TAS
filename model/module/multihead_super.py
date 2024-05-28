f1b4094bdffcd359246746bf33a10b6079c6cd60
wanyao <wanyaocsg17@icloud.com>
2024-05-28 14:35:12 +0800

    Update multihead_super.py

--- model/module/multihead_super.py
+++ model/module/multihead_super.py
@@ -178,11 +178,11 @@ class AttentionSuper(nn.Module):
             attn = attn + (q.permute(2, 0, 1, 3).reshape(N, self.sample_num_heads * B, -1) @ r_p_k.transpose(2, 1)) \
                 .transpose(1, 0).reshape(B, self.sample_num_heads, N, N) * self.sample_scale
 
-        # self.attentions = attn  # Save attention weights for later use
+        self.attentions = attn  # Save attention weights for later use
         
         attn = attn.softmax(dim=-1)
 
-        self.attentions = attn  # Save attention weights for later use
+        # self.attentions = attn  # Save attention weights for later use
 
         # score = self.diversity_score()
         # print('score: ', score.item())
