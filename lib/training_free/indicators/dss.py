87cea855a4f20b5bdc73ba17c48906cb8290bb95
wanyao <wanyaocsg17@icloud.com>
2024-06-03 14:51:17 +0800

    Update dss.py

--- lib/training_free/indicators/dss.py
+++ lib/training_free/indicators/dss.py
@@ -48,13 +48,13 @@ def compute_dss_per_weight(net, inputs, targets, mode, split_data=1, loss_fn=Non
         if isinstance(layer, nn.Linear) and 'qkv' not in layer._get_name() \
             and layer.out_features != layer.in_features and layer.out_features != 100 and layer.samples:
             if layer.samples['weight'].grad is not None:
-                print('MLP矩阵: ', layer.samples['weight'].shape)
+                # print('MLP矩阵: ', layer.samples['weight'].shape)
                 return torch.abs(layer.samples['weight'].grad * layer.samples['weight'])
             else:
                 return torch.zeros_like(layer.samples['weight'])
         elif isinstance(layer, torch.nn.Linear) and layer.out_features == 100:
             if layer.samples['weight'].grad is not None:
-                print('分类头矩阵: ', layer.samples['weight'].shape)
+                # print('分类头矩阵: ', layer.samples['weight'].shape)
                 return torch.abs(layer.samples['weight'].grad * layer.samples['weight'])
             else:
                 return torch.zeros_like(layer.samples['weight'])
