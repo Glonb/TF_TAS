54e37ca6e674f032583fbf5bf476ac9606be2c0d
wanyao <wanyaocsg17@icloud.com>
2024-06-12 10:00:32 +0800

    Update train_searched_result.sh

--- train_searched_result.sh
+++ train_searched_result.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python3 train.py --data-set CIFAR100 --num_workers 4 --gp --change_qkv --relative_position \
---mode retrain --model_type 'AUTOFORMER' --cfg './experiments/subnet_autoformer/TF_TAS-T-CIFAR100.yaml' --output_dir './OUTPUT/sample'
+python3 train.py --data-set CIFAR10 --num_workers 4 --gp --change_qkv --relative_position \
+--mode retrain --model_type 'AUTOFORMER' --cfg './experiments/subnet_autoformer/TF_TAS-T-CIFAR10.yaml' --output_dir './OUTPUT/sample'
 
 
