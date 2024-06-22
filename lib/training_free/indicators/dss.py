e2561b182b97fe3bb7f977aad3ba8da382fbd165
wanyao <wanyaocsg17@icloud.com>
2024-06-22 15:27:44 +0800

    Update dss.py

--- lib/training_free/indicators/dss.py
+++ lib/training_free/indicators/dss.py
@@ -42,6 +42,7 @@ def compute_dss_per_weight(net, inputs, targets, mode, split_data=1, loss_fn=Non
             or isinstance(layer, nn.Linear) and layer.out_features == layer.in_features and layer.samples:
             if layer.samples['weight'].grad is not None:
                 print('MSA矩阵：', layer.samples['weight'].shape)
+                print('forward result：', (nn.functional.linear(layer.samples['weight'], layer.samples['bias']) * layer.sample_scale).shape)
                 return torch.abs(
                     torch.norm(layer.samples['weight'].grad, 'nuc') * torch.norm(layer.samples['weight'], 'nuc'))
             else:
