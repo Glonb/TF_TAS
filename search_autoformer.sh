f5bca552a1e03751d6e348db2c84468014a81a73
wanyao <wanyaocsg17@icloud.com>
2024-06-25 11:30:50 +0800

    Update search_autoformer.sh

--- search_autoformer.sh
+++ search_autoformer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python3 search_autoformer.py --data-path /mnt/workspace --data-set CIFAR10 --num_workers 4  --indicator-name mine --gp \
+python3 search_autoformer.py --data-path /mnt/workspace --data-set CIFAR10 --num_workers 4  --indicator-name dss --gp \
 --param-limits 23 --change_qkv --relative_position --cfg './experiments/search_space/space-T.yaml' --output_dir './OUTPUT/search'
 
 
