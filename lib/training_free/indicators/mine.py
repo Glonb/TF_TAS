58a8b6198b8de60bbd11d78bf4034e2f33855587
wanyao <wanyaocsg17@icloud.com>
2024-07-09 16:02:05 +0800

    Update mine.py

--- lib/training_free/indicators/mine.py
+++ lib/training_free/indicators/mine.py
@@ -33,7 +33,7 @@ def compute_mine_per_weight(net, inputs, targets, mode, split_data=1, loss_fn=No
 
     # 通过retain_grad()保留注意力头的梯度
     for name, param in net.named_parameters():
-        print('name -> param: ', name, param)
+        print('name -> param: ', name, ' ->param ', param)
         # if 'attn' in name:  # 假设注意力头的参数名称包含'attn'
         #     param.retain_grad()
     torch.sum(output).backward()
