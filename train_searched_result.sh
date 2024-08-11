e136bfa25df45c4d1ba27aabb3f0274c290d93d0
wanyao <wanyaocsg17@icloud.com>
2024-08-11 14:41:08 +0800

    Update train_searched_result.sh

--- train_searched_result.sh
+++ train_searched_result.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python3 train.py --data-path ~/autodl-tmp/MuReD/label_one --data-set MuReD --num_workers 4 --gp --change_qkv --relative_position \
+python3 train.py --data-path ~/autodl-tmp/MuReD/label_one --data-set MuReD --num_workers 8 --gp --change_qkv --relative_position \
 --mode retrain --model_type 'AUTOFORMER' --cfg './experiments/subnet_autoformer/TF_TAS-T-MuReD.yaml' --output_dir './OUTPUT/sample'
 
 
